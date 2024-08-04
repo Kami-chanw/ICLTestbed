66372e9b428926d7d77294fe150a18bf987bcf71
Kami-chanw <865710157@qq.com>
2024-08-04 11:38:27 +0800

    finish pipeline

--- tests/vqa_accuracy/__init__.py
+++
@@ -1,7 +0,0 @@
-from .vqa import VQA
-from .vqaEval import VQAEval
-import random
-
-def generate_fake_result(questions_path, annotations_path):
-    vqa = VQA(annotations_path, questions_path)
-    
